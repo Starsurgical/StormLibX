1cc7b16079d15c97146ef0f283074b0b257f0e2d
Adam Heinermann <aheinerm@gmail.com>
2025-11-13 15:59:40 -0800

    Stub SLog functions

--- include/SLog.h
+++ include/SLog.h
@@ -1,8 +1,23 @@
 #ifndef __STORMLIBX_SLOG_H__
 #define __STORMLIBX_SLOG_H__
 
+#include "StormTypes.h"
 
-extern "C" {
-}
+
+DECLARE_HANDLE(HSLOCKEDLOG);
+DECLARE_HANDLE(HSLOG);
+
+// @541
+void STORMAPI SLogClose(HSLOG log);
+
+// @542
+BOOL STORMAPI SLogCreate(const char *filename, BYTE flags, HSLOG *log);
+
+// @548
+void STORMCDECL SLogWrite(HSLOG log, const char *format, ...);
+
+// @550
+void STORMAPI SLogSetIndent(HSLOG log, int indent);
+
 
 #endif
--- src/SLog.cpp
+++ src/SLog.cpp
@@ -1,2 +1,21 @@
 #include "SLog.h"
 
+static int s_sequence;
+
+
+// @541
+void STORMAPI SLogClose(HSLOG log) {
+}
+
+// @542
+BOOL STORMAPI SLogCreate(const char *filename, BYTE flags, HSLOG *log) {
+  return TRUE;
+}
+
+// @548
+void STORMCDECL SLogWrite(HSLOG log, const char *format, ...) {
+}
+
+// @550
+void STORMAPI SLogSetIndent(HSLOG log, int indent) {
+}
