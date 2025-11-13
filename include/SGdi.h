1e6f95e83d86abbf126f22357e236b5174efdfd4
Adam Heinermann <aheinerm@gmail.com>
2025-11-13 12:38:20 -0800

    SGdiSetPitch, SGdiSetTargetDimensions, SGdi stubs

--- include/SGdi.h
+++ include/SGdi.h
@@ -8,7 +8,26 @@ DECLARE_HANDLE(HSGDIOBJ);
 DECLARE_HANDLE(HSGDIFONT);
 
 
-extern "C" {
-}
+// @383
+BOOL STORMAPI SGdiDeleteObject(HSGDIOBJ handle);
+
+// @385
+BOOL STORMAPI SGdiExtTextOut(std::uint8_t* videobuffer, int x, int y, RECT* rect, std::uint32_t color, int textcoloruse, int bkgcoloruse, const char* string, int chars = -1);
+
+// @386
+BOOL STORMAPI SGdiImportFont(HFONT windowsfont, HSGDIFONT *handle);
+
+// @389
+BOOL STORMAPI SGdiSelectObject(HSGDIOBJ handle);
+
+// @390
+BOOL STORMAPI SGdiSetPitch(int pitch);
+
+// @392
+BOOL STORMAPI SGdiSetTargetDimensions(int width, int height, int bitdepth, int pitch);
+
+// @393
+BOOL STORMAPI SGdiGetTextExtent(const char* string, int chars, LPSIZE size);
+
 
 #endif
--- src/SGdi.cpp
+++ src/SGdi.cpp
@@ -1,2 +1,67 @@
 #include "SGdi.h"
+#include "SDraw.h"
+#include <vector>
 
+
+static std::int32_t s_screencx;
+static std::int32_t s_screency;
+static std::int32_t s_screenbpp;
+static std::int32_t s_pitch;
+static BOOL s_screenbppshift;
+static std::vector<std::int32_t> s_pitchtable;
+
+
+// @383
+BOOL STORMAPI SGdiDeleteObject(HSGDIOBJ handle) {
+  return TRUE;
+}
+
+// @385
+BOOL STORMAPI SGdiExtTextOut(std::uint8_t* videobuffer, int x, int y, RECT* rect, std::uint32_t color, int textcoloruse, int bkgcoloruse, const char* string, int chars = -1) {
+  return TRUE;
+}
+
+// @386
+BOOL STORMAPI SGdiImportFont(HFONT windowsfont, HSGDIFONT *handle) {
+  return TRUE;
+}
+
+// @389
+BOOL STORMAPI SGdiSelectObject(HSGDIOBJ handle) {
+  return TRUE;
+}
+
+// @390
+BOOL STORMAPI SGdiSetPitch(int pitch) {
+  STORM_VALIDATE_BEGIN;
+  STORM_VALIDATE(pitch > 0);
+  STORM_VALIDATE_END;
+
+  SDrawGetScreenSize(&s_screencx, &s_screency, &s_screenbpp);
+  return SGdiSetTargetDimensions(s_screencx, s_screency, s_screenbpp, pitch);
+}
+
+// @392
+BOOL STORMAPI SGdiSetTargetDimensions(int width, int height, int bitdepth, int pitch) {
+  s_screencx = width;
+  s_screency = height;
+  s_screenbpp = bitdepth;
+  s_screenbppshift = bitdepth == 16;
+
+  if (pitch != s_pitch || height != s_pitchtable.size()) {
+    s_pitch = pitch;
+
+    s_pitchtable.clear();
+    int offset = 0;
+    for (int i = 0; i < height; offset += pitch) {
+      s_pitchtable.push_back(offset);
+    }
+  }
+
+  return TRUE;
+}
+
+// @393
+BOOL STORMAPI SGdiGetTextExtent(const char* string, int chars, LPSIZE size) {
+  return TRUE;
+}
