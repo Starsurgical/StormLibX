eb9aeaa2e5db08b57a454bb7de08cbb340316fa6
Adam Heinermann <aheinerm@gmail.com>
2025-11-12 18:26:52 -0800

    SDlgSetCursor, SDlgSetMessageLoopThrottle

--- include/SDlg.h
+++ include/SDlg.h
@@ -1,7 +1,13 @@
 #ifndef __STORMLIBX_SDLG_H__
 #define __STORMLIBX_SDLG_H__
 
+#include "StormTypes.h"
+
 extern "C" {
+
+  BOOL STORMAPI SDlgSetCursor(HWND window, HCURSOR cursor, DWORD id, HCURSOR *oldcursor);
+
+  int STORMAPI SDlgSetMessageLoopThrottle(int throttle);
 }
 
 #endif
--- src/SDlg.cpp
+++ src/SDlg.cpp
@@ -1,1 +1,18 @@
 #include "SDlg.h"
+#include <SDL_mouse.h>
+#include <SDL_timer.h>
+
+
+int s_throttleDialogMessageLoop;
+
+
+BOOL STORMAPI SDlgSetCursor(HWND window, HCURSOR cursor, DWORD id, HCURSOR *oldcursor) {
+  if (oldcursor) *oldcursor = reinterpret_cast<HCURSOR>(SDL_GetCursor());
+  SDL_SetCursor(reinterpret_cast<SDL_Cursor*>(cursor));
+  return TRUE;
+}
+
+int STORMAPI SDlgSetMessageLoopThrottle(int throttle) {
+  s_throttleDialogMessageLoop = throttle;
+  return throttle;
+}
